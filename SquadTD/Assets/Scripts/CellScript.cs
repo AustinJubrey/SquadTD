d24d0a6f8f81534c003e5c6436fb9fc1306eccd2
Austin <austinjubrey@gmail.com>
2018-02-28 10:06:39 -0500

    [ADDED] basic tile placement and path creation

--- SquadTD/Assets/Scripts/CellScript.cs
+++ SquadTD/Assets/Scripts/CellScript.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CellScript : MonoBehaviour {
+
+    public int state, id;
+
+    public Color backColor, pathColor;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void SetState(int i)
+    {
+        state = i;
+
+        if (i == 0)
+        {
+            GetComponent<Image>().color = backColor;
+        }
+
+        if (i == 1)
+        {
+            GetComponent<Image>().color = pathColor;
+        }
+    }
 }
---
+++ SquadTD/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour {
+
+    public int cellCount;
+
+    int[] pathID = { 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162,
+    187, 212, 237, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275};
+
+    List<CellScript> AllCells = new List<CellScript>();
+
+    public GameObject cellPref;
+    public Transform cellGroup;
+
+	// Use this for initialization
+	void Start () {
+        CreateCells();
+        CreatePath();
+	}
+
+    void CreateCells()
+    {
+        for (int i = 0; i < cellCount; i++)
+        {
+            GameObject tempCell = Instantiate(cellPref);
+            tempCell.transform.SetParent(cellGroup, false);
+            tempCell.GetComponent<CellScript>().id = i + 1;
+            tempCell.GetComponent<CellScript>().SetState(0);
+            AllCells.Add(tempCell.GetComponent<CellScript>());
+        }
+    }
+
+    void CreatePath()
+    {
+        for (int i = 0; i < pathID.Length; i++)
+        {
+            AllCells[pathID[i] - 1].SetState(1);
+        }
+    }
+}
